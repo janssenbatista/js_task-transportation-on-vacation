5259ac7ef4670f5c22506e8c531344fa9a36b042
Janssen Batista <batistajanssen.dev@gmail.com>
2025-03-21 20:05:09 -0300

    add task resolution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM = 3;
+  const MID_TERM_DISCOUNT = 20;
+  const CAR_COST_PER_DAY = 40;
+
+  if (days >= LONG_TERM) {
+    return days * CAR_COST_PER_DAY - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MID_TERM) {
+    return days * CAR_COST_PER_DAY - MID_TERM_DISCOUNT;
+  }
+
+  return days * CAR_COST_PER_DAY;
 }
 
 module.exports = calculateRentalCost;
